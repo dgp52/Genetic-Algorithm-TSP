7b028660b0801534b8195bbaa9e2111c1c58f977
dgp52 <deeppatel19940@gmail.com>
2017-11-20 19:57:09 -0500

    added a controller file and fxml file for view

--- GeneticAlgorithmTSP/src/controller/Main.java
+++ GeneticAlgorithmTSP/src/controller/Main.java
@@ -1,9 +1,38 @@
 package controller;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+public class Main extends Application{
+
+	/**
+	 * primary stag
+	 */
+	public static Stage primaryStage;
+
+	@Override
+	public void start(Stage primaryStage) {
+	    Main.primaryStage = primaryStage;
+		try {
+			Pane root = (Pane) FXMLLoader.load(getClass().getResource("view/GeneticAlgoTSP.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Genetic Algorithm TSP");
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param args String
+	 */
 	public static void main(String[] args) {
-		System.out.println("Main");
+		launch(args);
 	}
 
 }
