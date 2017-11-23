18ca61f86d32ca1f30124a6864ecfab0b4ac6693
dgp52 <deeppatel19940@gmail.com>
2017-11-23 13:27:47 -0500

    UI splitpane and draw points

--- GeneticAlgorithmTSP/src/controller/GeneticAlgoTSPController.java
+++ GeneticAlgorithmTSP/src/controller/GeneticAlgoTSPController.java
@@ -1,17 +1,41 @@
 package controller;
 
+
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
-
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 public class GeneticAlgoTSPController {
 	
 	@FXML
 	Canvas brutecanvas;
 	
 	@FXML
+	AnchorPane bparent;
+	
+	@FXML
 	protected void initialize() {
-		System.out.println(brutecanvas.getId());
-		brutecanvas.setStyle("-fx-background-color: red");
+		//Resize canvas to parent size
+		brutecanvas.widthProperty().bind(bparent.widthProperty()); 
+		brutecanvas.heightProperty().bind(bparent.heightProperty()); 
+		brutecanvas.addEventHandler(MouseEvent.MOUSE_PRESSED, 
+                new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+            	double x = event.getX();
+            	double y = event.getY();
+            	GraphicsContext gc = brutecanvas.getGraphicsContext2D();
+        		draw(gc,x,y);
+            }
+        });
+	}
+	
+	 private void draw(GraphicsContext gc, double x, double y) {
+		    gc.setFill(Color.WHITE);
+		    gc.fillOval(x-5, y-5, 10, 10);
 	}
 
 }
