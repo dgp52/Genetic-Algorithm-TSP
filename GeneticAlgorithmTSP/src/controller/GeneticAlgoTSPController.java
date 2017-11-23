d7bcaff1e2a3d58932919c5a6c113eff51e538b7
dgp52 <deeppatel19940@gmail.com>
2017-11-23 17:00:25 -0500

    draw path when points are being added

--- GeneticAlgorithmTSP/src/controller/GeneticAlgoTSPController.java
+++ GeneticAlgorithmTSP/src/controller/GeneticAlgoTSPController.java
@@ -1,14 +1,26 @@
 package controller;
 
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import model.Point;
+import model.Points;
 public class GeneticAlgoTSPController {
+	
+	private Points points;
+	private GraphicsContext brutegc, geneticgc;
+	public static final int DRAW_CENTER = 5;
 	
 	@FXML
 	Canvas brutecanvas, geneticcanvas;
@@ -17,21 +29,31 @@ public class GeneticAlgoTSPController {
 	AnchorPane bparent, gparent;
 	
 	@FXML
+	Button btn;
+	
+	@FXML
 	protected void initialize() {
 		resizeCanvasToParent();
-		
-		brutecanvas.addEventHandler(MouseEvent.MOUSE_PRESSED, 
-                new EventHandler<MouseEvent>(){
+		initializePoints();
+		initializeGraphicsContext();
+		brutecanvas.addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>(){
             @Override
             public void handle(MouseEvent event) {
-            	double x = event.getX();
-            	double y = event.getY();
-            	GraphicsContext brutegc = brutecanvas.getGraphicsContext2D();
-            	GraphicsContext geneticgc = geneticcanvas.getGraphicsContext2D();
-        		drawBrute(brutegc,x,y);
-        		drawGenetic(geneticgc,x,y);
+            	points.addPoint(new Point(event.getX()-DRAW_CENTER, event.getY()-DRAW_CENTER));
+        		drawBrute(brutegc,new Point(event.getX()-DRAW_CENTER, event.getY()-DRAW_CENTER));
+        		drawGenetic(geneticgc,new Point(event.getX()-DRAW_CENTER, event.getY()-DRAW_CENTER));
+        		drawPath();
             }
         });
+	}
+	
+	private void initializePoints() {
+		points = new Points();
+	}
+	
+	private void initializeGraphicsContext() {
+		brutegc = brutecanvas.getGraphicsContext2D();
+    	geneticgc = geneticcanvas.getGraphicsContext2D();
 	}
 	
 	private void resizeCanvasToParent () {
@@ -43,14 +65,27 @@ public class GeneticAlgoTSPController {
 		geneticcanvas.heightProperty().bind(gparent.heightProperty()); 
 	}
 	
-	private void drawBrute(GraphicsContext gc, double x, double y) {
+	private void drawBrute(GraphicsContext gc, Point point) {
 	    gc.setFill(Color.WHITE);
-	    gc.fillOval(x-5, y-5, 10, 10);
+	    gc.setStroke(Color.WHITE);
+	    gc.setLineWidth(3);
+	    gc.fillOval(point.getX(),point.getY(), 10, 10);
 	}
 	
-	 private void drawGenetic(GraphicsContext gc, double x, double y) {
-	    gc.setFill(Color.WHITE);
-	    gc.fillOval(x-5, y-5, 10, 10);
+	private void drawPath() {
+		brutegc.strokePolyline(points.getAllx(), points.getAlly(), points.getSize());
 	}
+	
+	 private void drawGenetic(GraphicsContext gc, Point point) {
+	    gc.setFill(Color.WHITE);
+	    gc.fillOval(point.getX(), point.getY(), 10, 10);
+	}
+	 
+	 public void handle(ActionEvent handler) throws IOException, NoSuchAlgorithmException {
+		Button b = (Button) handler.getSource();
+		if (b == btn) {
+			points.printPoints();
+		}
+	 }
 
 }
--- GeneticAlgorithmTSP/src/model/Points.java
+++ GeneticAlgorithmTSP/src/model/Points.java
@@ -1,8 +1,9 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+import controller.GeneticAlgoTSPController;
 
 public class Points {
 	private List<Point> points;
@@ -15,10 +16,33 @@ public class Points {
 		points.add(point);
 	}
 	
+	public double[] getAllx() {
+		double x[] = new double[points.size()];
+		for(int i =0; i < x.length; i++) {
+			Point p = points.get(i);
+			x[i] = p.getX()+GeneticAlgoTSPController.DRAW_CENTER;
+		}
+		return x;
+	}
+	
+	public double[] getAlly() {
+		double y[] = new double[points.size()];
+		for(int i =0; i < y.length; i++) {
+			Point p = points.get(i);
+			y[i] = p.getY()+GeneticAlgoTSPController.DRAW_CENTER;
+		}
+		return y;
+	}
+	
+	public int getSize() {
+		return points.size();
+	}
+	
 	public void printPoints() {
 		for(Point p: points) {
-			System.out.print(p.toString());
+			System.out.print("("+p.toString()+")");
 		}
+		System.out.println("");
 	}
 
 }
